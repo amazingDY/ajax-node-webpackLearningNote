41347a93bb734b97b716d6c2f7d057d391ee3603
dong.yu <1526386823@qq.com>
2024-03-21 17:56:49 +0800

    ajax进阶以及node基础笔记提交

---
+++ 1笔记/2node&webpack笔记/node笔记/01_node简介.js
@@ -0,0 +1,13 @@
+// node环境与浏览器环境最大的区别:Node.js 环境中没有 BOM 和 DOM，但是也用 JS 语法
+
+// 目标：编写 js 代码，在 Node 环境下使用 node 命令来独立执行
+console.log('Hello, Node.js')
+for (let i = 0; i < 3; i++) {
+  console.log(6)
+}
+//  node 01.js
+// 输出:
+// Hello, Node.js
+// 6
+// 6
+// 6
